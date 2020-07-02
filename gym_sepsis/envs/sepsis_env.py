c1faf8798a05170ca4cbf0731e0b3d96c4bc58fb
Shusheng Xu <xssstory@gmail.com>
2020-07-02 18:39:58 +0800

    update env

--- gym_sepsis/envs/sepsis_env.py
+++ gym_sepsis/envs/sepsis_env.py
@@ -6,6 +6,11 @@ import os
 from collections import deque
 import pandas as pd
 from gym import spaces
+import torch
+
+import tensorflow as tf
+gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options)) 
 
 STATE_MODEL = "model/sepsis_states.model"
 TERMINATION_MODEL = "model/sepsis_termination.model"
@@ -37,15 +42,18 @@ class SepsisEnv(gym.Env):
     """
     metadata = {'render.modes': ['ansi']}
 
-    def __init__(self, starting_state=None, verbose=False):
+    def __init__(self, args, starting_state=None, verbose=False):
         module_path = os.path.dirname(__file__)
         self.verbose = verbose
         self.state_model = keras.models.load_model(os.path.join(module_path, STATE_MODEL))
         self.termination_model = keras.models.load_model(os.path.join(module_path, TERMINATION_MODEL))
         self.outcome_model = keras.models.load_model(os.path.join(module_path, OUTCOME_MODEL))
         self.starting_states = np.load(os.path.join(module_path, STARTING_STATES_VALUES))['sepsis_starting_states']
-        self.seed()
-        self.action_space = spaces.Discrete(24)
+        self.state_buffer = deque([], maxlen=args.history_length)
+        self.window = args.history_length
+        self.seed(args.seed)
+        # self.action_space = spaces.Discrete(24)
+        self.device = args.device
 
         # use a pixel to represent next state
         self.observation_space = spaces.Box(low=0, high=NUM_ACTIONS, shape=(NUM_FEATURES-2, 1, 1),
@@ -94,15 +102,21 @@ class SepsisEnv(gym.Env):
         self.state_idx += 1
         self.rewards.append(reward)
         self.dones.append(done)
-        return self.s, reward, done, {"prob" : 1}
+        self.state_buffer.append(torch.tensor(self.s, device=self.device, dtype=torch.float32))
+        return torch.stack(list(self.state_buffer), 0), reward, done, {"prob" : 1}
+
+    def _reset_buffer(self):
+        for _ in range(self.window):
+            self.state_buffer.append(torch.zeros(NUM_FEATURES-2, 1, 1, device=self.device))
 
     def reset(self, starting_state=None):
         self.rewards = []
         self.dones = []
         self.state_idx = 0
         self.memory = deque([np.zeros(shape=[NUM_FEATURES])] * 10, maxlen=10)
+        self._reset_buffer()
         if starting_state is None:
-            self.s = self.starting_states[np.random.randint(0, len(self.starting_states))][:-1]
+            self.s = self.starting_states[self.rng.randint(0, len(self.starting_states))][:-1]
         else:
             self.s = starting_state
 
@@ -111,12 +125,24 @@ class SepsisEnv(gym.Env):
 
         if self.verbose:
             print("starting state:", self.s)
-        return self.s
+        self.state_buffer.append(torch.tensor(self.s, device=self.device, dtype=torch.float32))
+        return torch.stack(list(self.state_buffer), 0)
 
     def seed(self, seed=None):
-        seed = seeding.np_random(seed)
+        self.rng, seed = seeding.np_random(seed)
         return [seed]
 
     def render(self, mode='ansi'):
         df = pd.DataFrame(self.memory, columns=features, index=range(0, 10))
         print(df)
+
+  # Uses loss of life as terminal signal
+    def train(self):
+        self.training = True
+
+  # Uses standard terminal signal
+    def eval(self):
+        self.training = False
+    
+    def action_space(self):
+        return NUM_ACTIONS
